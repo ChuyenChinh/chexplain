bfda604b559d7224e3187d992a3e235676dcf031
Chuyen Chinh <chinhchuyen71@gmail.com>
2026-09-08 22:47:23 +0700

    small bugs on reading label_csv

--- dataset.py
+++ dataset.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 class CustomXRayDataset(Dataset):
     def __init__(self,img_dir='None',label_dir='None',transforms='None'):
-        df = pd.read_csv('label_dir')
+        df = pd.read_csv(label_dir)
         df['Path'] = df['Path'].str.replace('CheXpert-v1.0-small','chexpert')
         
         labels_name = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Pleural Effusion']
