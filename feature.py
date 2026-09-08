4aca405dcc529e396cea3216f09edd5caa637617
Chuyen Chinh <chinhchuyen71@gmail.com>
2026-09-09 00:25:18 +0700

    ...

--- feature.py
+++ feature.py
@@ -24,8 +24,8 @@ def get_dataloader():
           for x in ['train','valid']}
     
     dataloader = {
-        'train' : DataLoader(dataset['train'],batch_size=64,shuffle=False,sampler=DistributedSampler(dataset['train']),num_workers=1,pin_memory=False),
-        'valid' : DataLoader((dataset['valid']),batch_size=32,shuffle=False,sampler=DistributedSampler(dataset['valid'],shuffle=False),num_workers=1,pin_memory=False)
+        'train' : DataLoader(dataset['train'],batch_size=64,shuffle=False,sampler=DistributedSampler(dataset['train']),num_workers=0,pin_memory=False),
+        'valid' : DataLoader((dataset['valid']),batch_size=32,shuffle=False,sampler=DistributedSampler(dataset['valid'],shuffle=False),num_workers=0,pin_memory=False)
     }
     
     return dataloader
